f37535588a871ceb202b3881c1e2042d3712e603
Karolis Kerpys <karolis.kerpys@gmail.com>
2018-03-17 23:25:10 +0200

    update

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require('express');
 const app = express();
 const tools = require('./json-data/tools');
 
-app.get('/tools', (req,res)=>{
-    res.status(200).json({tools})
+app.get('/tools', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+      'Access-Control-Allow-Origin',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.status(200).json({tools})
 });
 
 const PORT = process.env.PORT || 9000;
 
-app.listen(PORT, ()=>{
-    console.log(`server is up and running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`server is up and running on port ${PORT}`);
 });
