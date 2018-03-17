276163a09235e5ff2663872560582780bbf872d6
Karolis Kerpys <karolis.kerpys@gmail.com>
2018-03-17 23:49:04 +0200

    fix

--- app.js
+++ app.js
@@ -3,15 +3,7 @@ const app = express();
 const tools = require('./json-data/tools');
 
 app.use(function(req, res, next) {
-  var allowedOrigins = ['https://enigmatic-cliffs-25405.herokuapp.com/tools', 'http://localhost:80', 'http://127.0.0.1:9000', 'http://localhost:9000'];
-  var origin = req.headers.origin;
-  if(allowedOrigins.indexOf(origin) > -1){
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', true);
+
   return next();
 });
 
