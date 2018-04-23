1f0cdc0d7c93da5ff312148d61da60b113130fc4
Karolis Kerpys <karolis.kerpys@gmail.com>
2018-04-24 01:14:07 +0300

    questions

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const tools = require('./json-data/tools');
 const menu = require('./json-data/menu');
+const questions = require('./json-data/questions');
 const cors = require('cors');
 
 
@@ -12,6 +13,9 @@ app.get('/tools', (req, res) => {
 });
 app.get('/menu', (req,res)=>{
     res.status(200).json({menu})
+});
+app.get('/questions', (req,res)=>{
+  res.status(200).json({questions})
 });
 
 const PORT = process.env.PORT || 9000;
---
+++ json-data/questions.js
@@ -0,0 +1,30 @@
+module.exports = {
+  html: [
+    {
+      question: 'What does HTML stand for?',
+      answers: [
+        'Hyperlinks and Text Markup Language',
+        'Hyper Text Markup Language',
+        'Home Tool Markup Language'
+      ],
+      correct:0
+    },
+    {
+      question:'How can you open a link in a new tab/browser window?',
+      answers:[
+          ' <a href="url" target="new">',
+          '<a href="url" new>',
+          '<a href="url" target="_blank">'
+      ],
+      correct:2
+    },
+    {
+      question:'Inline elements are normally displayed without starting a new line',
+      answers:[
+        'true',
+        'false',
+      ],
+      correct:0
+    }
+  ]
+};
